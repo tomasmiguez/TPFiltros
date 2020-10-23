8aeb2dbc720e40124e7a4361bec4ff564f832a2b
Tomas Miguez <tomasmiguez99@gmail.com>
2020-10-23 16:38:42 -0300

    Funca correctamente ColorBordes, pasa los tests. :D

--- src/filters/ColorBordes_asm.asm
+++ src/filters/ColorBordes_asm.asm
@@ -9,7 +9,7 @@ MASK_OR: DD 0xFF000000, 0xFF000000, 0xFF000000, 0xFF000000
 
 section .text
 
-; gdb --args ../build/tp2 ColorBordes -i asm ../img/Seven.bmp
+; gdb --args ../build/tp2 ColorBordes -i asm ../Misery.32x16.bmp
 ; Cada iteracion cargamos todo esto, y procesamos los 2 pixeles p1 y p2
 ; | TL | T1 | T2 | TR |
 ; | ML | P1 | P2 | MR |
@@ -52,41 +52,58 @@ ColorBordes_asm: ; RDI <- src / RSI <- dst / RDX <- width / RCX <- height
 .loop:
     MOV R9, RBX
     MOVDQU XMM0, [R9]   ; XMM0  <- [ TR T2 T1 TL ]
+    MOVDQA XMM1, XMM0   ; XMM1  <- [ TR T2 T1 TL ]
+    PSRLDQ XMM1, 8      ; XMM1  <- [ 0  0  TR T2 ]
+    PMOVZXBW XMM0, XMM0 ; XMM0  <- [ T1 TL ]
+    PMOVZXBW XMM1, XMM1 ; XMM1  <- [ TR T2 ]
     LEA R9, [R9+4*R8]
-    MOVDQU XMM1, [R9]   ; XMM1  <- [ MR P2 P1 ML ]
-    LEA R9, [R9+4*R8]
-    MOVDQU XMM2, [R9]   ; XMM2  <- [ BR B2 B1 BL ]
-
-    MOVDQA XMM3, XMM0   ; XMM3  <- [ TR T2 T1 TL ]
-    PSUBB XMM3, XMM2    ; XMM3  <- [ TR-BR T2-B2 T1-B1 TL-BL ]
-    PABSB XMM3, XMM3    ; XMM3  <- [ |TR-BR| |T2-B2| |T1-B1| |TL-BL| ]
-
-    MOVDQA XMM15, XMM3  ; XMM15 <- [ |TR-BR| |T2-B2| |T1-B1| |TL-BL| ]
-    PSRLDQ XMM3, 4      ; XMM3  <- [ 0       |TR-BR| |T2-B2| |T1-B1| ]
-    PADDSB XMM15, XMM3  ; XMM15 <- [ X X |T2-B2|+|T1-B1| |T1-B1|+|TL-BL| ]
-    PSRLDQ XMM3, 4      ; XMM3  <- [ 0       0       |TR-BR| |T2-B2| ]
-    PADDSB XMM15, XMM3  ; XMM15 <- [ X X |TR-BR|+|T2-B2|+|T1-B1| |T2-B2|+|T1-B1|+|TL-BL| ]
-
-    MOVDQA XMM3, XMM0   ; XMM3  <- [ TR T2 T1 TL ]
-    PSRLDQ XMM3, 8      ; XMM3  <- [ 0  0  TR T2 ]
-    PSUBB XMM3, XMM0    ; XMM3  <- [ X X TR-T1 T2-TL ]
-    PABSB XMM3, XMM3    ; XMM3  <- [ X X |TR-T1| |T2-TL| ]
-    PADDSB XMM15, XMM3  ; XMM15 <- [ X X (|TR-T1|)+(|TR-BR|+|T2-B2|+|T1-B1|) (|T2-TL|)+(|T2-B2|+|T1-B1|+|TL-BL|) ]
-
-    MOVDQA XMM3, XMM1   ; XMM3  <- [ MR P2 P1 ML ]
+    MOVDQU XMM2, [R9]   ; XMM2  <- [ MR P2 P1 ML ]
+    MOVDQA XMM3, XMM2   ; XMM3  <- [ MR P2 P1 ML ]
     PSRLDQ XMM3, 8      ; XMM3  <- [ 0  0  MR P2 ]
-    PSUBB XMM3, XMM1    ; XMM3  <- [ X X P1-MR P2-ML ]
-    PABSB XMM3, XMM3    ; XMM3  <- [ X X |P1-MR| |P2-ML| ]
-    PADDSB XMM15, XMM3  ; XMM15 <- [ X X (|P1-MR|+|TR-T1|)+(|TR-BR|+|T2-B2|+|T1-B1|) (|P2-ML|+|T2-TL|)+(|T2-B2|+|T1-B1|+|TL-BL|) ]
+    PMOVZXBW XMM2, XMM2 ; XMM2  <- [ P1 ML ]
+    PMOVZXBW XMM3, XMM3 ; XMM3  <- [ MR P2 ]
+    LEA R9, [R9+4*R8]
+    MOVDQU XMM4, [R9]   ; XMM4  <- [ BR B2 B1 BL ]
+    MOVDQA XMM5, XMM4   ; XMM5  <- [ BR B2 B1 BL ]
+    PSRLDQ XMM5, 8      ; XMM5  <- [ 0  0  BR B2 ]
+    PMOVZXBW XMM4, XMM4 ; XMM4  <- [ B1 BL ]
+    PMOVZXBW XMM5, XMM5 ; XMM5  <- [ BR B2 ]
 
-    MOVDQA XMM3, XMM2   ; XMM3  <- []
-    PSRLDQ XMM3, 8      ; XMM3  <- []
-    PSUBB XMM3, XMM2    ; XMM3  <- []
-    PABSB XMM3, XMM3    ; XMM3  <- []
-    PADDSB XMM15, XMM3  ; XMM15 <- []
+    MOVDQA XMM6, XMM0   ; XMM6  <- [ T1 TL ]
+    PSUBSW XMM6, XMM1   ; XMM6  <- [ T1-TR TL-T2 ]
+    PABSW XMM6, XMM6    ; XMM6  <- [ |T1-TR| |TL-T2| ]
+    MOVDQA XMM15, XMM6  ; XMM15 <- [ |T1-TR| |TL-T2| ]
+ 
+    MOVDQA XMM7, XMM2   ; XMM7  <- [ P1 ML ]
+    PSUBSW XMM7, XMM3   ; XMM7  <- [ P1-MR ML-P2 ]
+    PABSW XMM7, XMM7    ; XMM6  <- [ |P1-MR| |ML-P2| ]
+    PADDSW XMM15, XMM7  ; XMM15 <- [ |T1-TR|+|P1-MR| |TL-T2|+|ML-P2| ]
+
+    MOVDQA XMM8, XMM4   ; XMM8  <- [ B1 BL ]
+    PSUBSW XMM8, XMM5   ; XMM8  <- [ B1-BR BL-B2]
+    PABSW XMM8, XMM8    ; XMM8  <- [ |B1-BR| |BL-B2| ]
+    PADDSW XMM15, XMM8  ; XMM15 <- [ |T1-TR|+|P1-MR|+|B1-BR| |TL-T2|+|ML-P2|+|BL-B2| ]
+    
+    MOVDQA XMM6, XMM0   ; XMM6  <- [ T1 TL ]
+    PSUBSW XMM6, XMM4   ; XMM6  <- [ T1-B1 TL-BL ]
+    PABSW XMM6, XMM6    ; XMM6  <- [ |T1-B1| |TL-BL| ]
+    PADDSW XMM15, XMM6  ; XMM15 <- [ (|T1-B1|)+(|T1-TR|+|P1-MR|+|B1-BR|) (|TL-BL|)+(|TL-T2|+|ML-P2|+|BL-B2|) ]
+
+    MOVDQA XMM7, XMM1   ; XMM7  <- [ TR T2 ]
+    PSUBSW XMM7, XMM5   ; XMM7  <- [ TR-BR T2-B2 ]
+    PABSW XMM7, XMM7    ; XMM7  <- [ |TR-BR| |T2-B2| ]
+    PADDSW XMM15, XMM7  ; XMM15 <- [ (|TR-BR|+|T1-B1|)+(|T1-TR|+|P1-MR|+|B1-BR|) (|T2-B2|+|TL-BL|)+(|TL-T2|+|ML-P2|+|BL-B2|) ]
+
+    PSRLDQ XMM6, 8      ; XMM6  <- [ 0 |T1-B1| ]
+    PADDSW XMM15, XMM6  ; XMM15 <- [ (|TR-BR|+|T1-B1|)+(|T1-TR|+|P1-MR|+|B1-BR|) (|T1-B1|+|T2-B2|+|TL-BL|)+(|TL-T2|+|ML-P2|+|BL-B2|) ]
+    
+    PSLLDQ XMM7, 8      ; XMM7  <- [ |T2-B2| 0 ]
+    PADDSW XMM15, XMM7  ; XMM15 <- [ (|T2-B2|+|TR-BR|+|T1-B1|)+(|T1-TR|+|P1-MR|+|B1-BR|) (|T1-B1|+|T2-B2|+|TL-BL|)+(|TL-T2|+|ML-P2|+|BL-B2|) ]
+
+    PACKUSWB XMM15, XMM15
 
     MOVDQA XMM10, [MASK_OR]
-    ;POR XMM15, XMM10    ; XMM15 <- [ X X X X X X X X FF P2R P2G P2B FF P1R P1G P1B ]
+    POR XMM15, XMM10    ; XMM15 <- [ X X X X X X X X FF P2R P2G P2B FF P1R P1G P1B ]
 
     MOVQ [R12], XMM15
 

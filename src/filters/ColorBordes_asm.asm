f1d092e5ccae629379cbfff77fd1b4aede98bb36
Tomas Miguez <tomasmiguez99@gmail.com>
2020-10-23 05:12:37 -0300

    Se agregaron los bordes blancos.

--- src/filters/ColorBordes_asm.asm
+++ src/filters/ColorBordes_asm.asm
@@ -7,7 +7,6 @@ ALIGN 16
 
 MASK_OR: DD 0xFF000000, 0xFF000000, 0xFF000000, 0xFF000000
 
-
 section .text
 
 ; gdb --args ../build/tp2 ColorBordes -i asm ../img/Seven.bmp
@@ -29,6 +28,7 @@ ColorBordes_asm: ; RDI <- src / RSI <- dst / RDX <- width / RCX <- height
     MOV RBX, RDI    ; RBX <- src
     MOV R12, RSI    ; R12 <- dst
     MOV R8, RDX     ; R8  <- width
+    MOV R10, RCX    ; R10 <- height
 
     MOV R15, R8     ; R15 <- width
     SUB R15, 2      ; R15 <- width-2
@@ -42,7 +42,6 @@ ColorBordes_asm: ; RDI <- src / RSI <- dst / RDX <- width / RCX <- height
     SAR R14, 1
     DEC R14         ; R14 <- (width/2)-1
 
-    MOV R10, RBX    ; Puntero a src
     MOV R11, R12    ; Puntero a resultado    
 
     MOV RCX, 0      ; Contador de iteracion
@@ -59,35 +58,35 @@ ColorBordes_asm: ; RDI <- src / RSI <- dst / RDX <- width / RCX <- height
     MOVDQU XMM2, [R9]   ; XMM2  <- [ BR B2 B1 BL ]
 
     MOVDQA XMM3, XMM0   ; XMM3  <- [ TR T2 T1 TL ]
-    PSUBB XMM3, XMM2   ; XMM3  <- [ TR-BR T2-B2 T1-B1 TL-BL ]
+    PSUBB XMM3, XMM2    ; XMM3  <- [ TR-BR T2-B2 T1-B1 TL-BL ]
     PABSB XMM3, XMM3    ; XMM3  <- [ |TR-BR| |T2-B2| |T1-B1| |TL-BL| ]
 
     MOVDQA XMM15, XMM3  ; XMM15 <- [ |TR-BR| |T2-B2| |T1-B1| |TL-BL| ]
     PSRLDQ XMM3, 4      ; XMM3  <- [ 0       |TR-BR| |T2-B2| |T1-B1| ]
     PADDSB XMM15, XMM3  ; XMM15 <- [ X X |T2-B2|+|T1-B1| |T1-B1|+|TL-BL| ]
-    PSRLDQ XMM9, 4      ; XMM9  <- [ 0       0       |TR-BR| |T2-B2| ]
+    PSRLDQ XMM3, 4      ; XMM3  <- [ 0       0       |TR-BR| |T2-B2| ]
     PADDSB XMM15, XMM3  ; XMM15 <- [ X X |TR-BR|+|T2-B2|+|T1-B1| |T2-B2|+|T1-B1|+|TL-BL| ]
 
     MOVDQA XMM3, XMM0   ; XMM3  <- [ TR T2 T1 TL ]
     PSRLDQ XMM3, 8      ; XMM3  <- [ 0  0  TR T2 ]
-    PSUBB XMM3, XMM0   ; XMM3  <- [ X X TR-T1 T2-TL ]
+    PSUBB XMM3, XMM0    ; XMM3  <- [ X X TR-T1 T2-TL ]
     PABSB XMM3, XMM3    ; XMM3  <- [ X X |TR-T1| |T2-TL| ]
     PADDSB XMM15, XMM3  ; XMM15 <- [ X X (|TR-T1|)+(|TR-BR|+|T2-B2|+|T1-B1|) (|T2-TL|)+(|T2-B2|+|T1-B1|+|TL-BL|) ]
 
-    MOVDQA XMM3, XMM1   ; XMM3  <- []
-    PSRLDQ XMM3, 8      ; XMM3  <- []
-    PSUBB XMM3, XMM1   ; XMM3  <- []
-    PABSB XMM3, XMM3    ; XMM3  <- []
-    PADDSB XMM15, XMM3  ; XMM15 <- []
+    MOVDQA XMM3, XMM1   ; XMM3  <- [ MR P2 P1 ML ]
+    PSRLDQ XMM3, 8      ; XMM3  <- [ 0  0  MR P2 ]
+    PSUBB XMM3, XMM1    ; XMM3  <- [ X X P1-MR P2-ML ]
+    PABSB XMM3, XMM3    ; XMM3  <- [ X X |P1-MR| |P2-ML| ]
+    PADDSB XMM15, XMM3  ; XMM15 <- [ X X (|P1-MR|+|TR-T1|)+(|TR-BR|+|T2-B2|+|T1-B1|) (|P2-ML|+|T2-TL|)+(|T2-B2|+|T1-B1|+|TL-BL|) ]
 
     MOVDQA XMM3, XMM2   ; XMM3  <- []
     PSRLDQ XMM3, 8      ; XMM3  <- []
-    PSUBB XMM3, XMM2   ; XMM3  <- []
+    PSUBB XMM3, XMM2    ; XMM3  <- []
     PABSB XMM3, XMM3    ; XMM3  <- []
     PADDSB XMM15, XMM3  ; XMM15 <- []
 
     MOVDQA XMM10, [MASK_OR]
-    POR XMM15, XMM10    ; XMM15 <- [ X X X X X X X X FF P2R P2G P2B FF P1R P1G P1B ]
+    ;POR XMM15, XMM10    ; XMM15 <- [ X X X X X X X X FF P2R P2G P2B FF P1R P1G P1B ]
 
     MOVQ [R12], XMM15
 
@@ -108,12 +107,32 @@ ColorBordes_asm: ; RDI <- src / RSI <- dst / RDX <- width / RCX <- height
     MOV RBX, R11
     MOV RCX, 0
 .loopPrimeraFila:
-    ;MOV qword[RBX], 0
-
+    MOV qword[RBX], -1
     ADD RBX, 8
     INC RCX
     CMP RCX, R8
     JL .loopPrimeraFila
+
+    SUB R10, 4
+    MOV RCX, 0
+.loopCostados:
+    MOV dword[RBX], -1
+    MOV R9, R8
+    DEC R9
+    LEA RBX, [RBX+4*R9]
+    MOV dword[RBX], -1
+    ADD RBX, 4
+    INC RCX
+    CMP RCX, R10
+    JL .loopCostados
+
+    MOV RCX, 0
+.loopUltimaFila:
+    MOV qword[RBX], -1
+    ADD RBX, 8
+    INC RCX
+    CMP RCX, R8
+    JL .loopUltimaFila
 
     ADD RSP, 8
     POP R15

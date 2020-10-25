39aad560e68293b4a514cb04005534bf463e05d2
Ignacio Dondo <dondoignacio@gmail.com>
2020-10-24 23:24:53 -0300

    Agrego exp2 en la carpeta correspondiente y borro cambios a imgFantasma original

--- experimentos/comparar/tests/src/filters/ImagenFantasma_exp2_c.c
+++ experimentos/comparar/tests/src/filters/ImagenFantasma_exp2_c.c
@@ -20,23 +20,29 @@ void ImagenFantasma_c(
 
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
+            
+            bgra_t pixelActual = src_matrix[i][j];
 
-            float rr = (float)src_matrix[i][j].r;
-            float gg = (float)src_matrix[i][j].g;
-            float bb = (float)src_matrix[i][j].b;
+            float rr = (float)pixelActual.r;
+            float gg = (float)pixelActual.g;
+            float bb = (float)pixelActual.b;
 
             int ii = i/2 + offsety;
             int jj = j/2 + offsetx;
 
-            float rrr = (float)src_matrix[ii][jj].r;
-            float ggg = (float)src_matrix[ii][jj].g;
-            float bbb = (float)src_matrix[ii][jj].b;
+            bgra_t pixelOff = src_matrix[ii][jj];
+
+            float rrr = (float)pixelOff.r;
+            float ggg = (float)pixelOff.g;
+            float bbb = (float)pixelOff.b;
 
             float b = (rrr + 2 * ggg + bbb)/4;
 
-            dst_matrix[i][j].r = SAT( rr * 0.9 + b/2 );
-            dst_matrix[i][j].g = SAT( gg * 0.9 + b/2 );
-            dst_matrix[i][j].b = SAT( bb * 0.9 + b/2 );
+            bgra_t* dstPixel = &(dst_matrix[i][j]);
+
+            dstPixel->r = SAT( rr * 0.9 + b/2 );
+            dstPixel->g = SAT( gg * 0.9 + b/2 );
+            dstPixel->b = SAT( bb * 0.9 + b/2 );   
         }
     }
 }

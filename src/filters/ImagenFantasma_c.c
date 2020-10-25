4073e634f97fa36be040eba471fe0bc58060ea84
Ignacio Dondo <dondoignacio@gmail.com>
2020-10-24 22:26:48 -0300

    agrego codigo exp imgFantasma

--- src/filters/ImagenFantasma_c.c
+++ src/filters/ImagenFantasma_c.c
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include "../tp2.h"
 #include "../helper/utils.h"
-
+/*
 void ImagenFantasma_c(
     uint8_t *src,
     uint8_t *dst,
@@ -37,6 +37,47 @@ void ImagenFantasma_c(
             dst_matrix[i][j].r = SAT( rr * 0.9 + b/2 );
             dst_matrix[i][j].g = SAT( gg * 0.9 + b/2 );
             dst_matrix[i][j].b = SAT( bb * 0.9 + b/2 );
+        }
+    }
+}
+*/
+
+void ImagenFantasma_c(
+    uint8_t *src,
+    uint8_t *dst,
+    int width,
+    int height,
+    int src_row_size,
+    int dst_row_size,
+    int offsetx,
+    int offsety)
+{
+    bgra_t (*src_matrix)[(src_row_size+3)/4] = (bgra_t (*)[(src_row_size+3)/4]) src;
+    bgra_t (*dst_matrix)[(dst_row_size+3)/4] = (bgra_t (*)[(dst_row_size+3)/4]) dst;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            
+            bgra_t pixelActual = src_matrix[i][j];
+
+            float rr = (float)pixelActual.r;
+            float gg = (float)pixelActual.g;
+            float bb = (float)pixelActual.b;
+
+            int ii = i/2 + offsety;
+            int jj = j/2 + offsetx;
+
+            bgra_t pixelOff = src_matrix[ii][jj];
+
+            float rrr = (float)pixelOff.r;
+            float ggg = (float)pixelOff.g;
+            float bbb = (float)pixelOff.b;
+
+            float b = (rrr + 2 * ggg + bbb)/4;
+
+            dst_matrix[i][j].r = SAT( rr * 0.9 + b/2 );
+            dst_matrix[i][j].g = SAT( gg * 0.9 + b/2 );
+            dst_matrix[i][j].b = SAT( bb * 0.9 + b/2 );   
         }
     }
 }

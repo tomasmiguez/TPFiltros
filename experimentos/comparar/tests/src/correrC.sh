48b3cc4584423a993aa1e2cf66cdda6e7232c85c
Tomas Miguez <tomasmiguez99@gmail.com>
2020-10-24 23:30:38 -0300

    Haciendo graficos.

--- experimentos/comparar/tests/src/correrC.sh
+++ experimentos/comparar/tests/src/correrC.sh
@@ -1,8 +1,8 @@
 > C.out
 for file in compImg/*
 do
-	build/tp2 ImagenFantasma -i c -t 100 $file 0 0 >> C.out
-	build/tp2 ColorBordes -i c -t 100 $file >> C.out
-	build/tp2 ReforzarBrillo -i c -t 100 $file 150 140 50 50 >> C.out
+	#build/tp2 ImagenFantasma -i c -t 200 $file 0 0 >> C.out
+	#build/tp2 ColorBordes -i c -t 200 $file >> C.out
+	build/tp2 ReforzarBrillo -i c -t 200 $file 150 140 50 50 >> C.out
 done
 rm *.bmp
--- experimentos/comparar/tests/src/filters/ReforzarBrillo_c.c
+++ experimentos/comparar/tests/src/filters/ReforzarBrillo_c.c
@@ -24,6 +24,45 @@ void ReforzarBrillo_c(
         for (int j = 0; j < width; j++) {
 
             int b = (src_matrix[i][j].r + 2*src_matrix[i][j].g + src_matrix[i][j].b)/4;
+        
+            if ( b > umbralSup ) {
+                dst_matrix[i][j].b = SAT(src_matrix[i][j].b+brilloSup);
+                dst_matrix[i][j].g = SAT(src_matrix[i][j].g+brilloSup);
+                dst_matrix[i][j].r = SAT(src_matrix[i][j].r+brilloSup);
+            } else if ( umbralInf > b ) {
+                dst_matrix[i][j].b = SAT(src_matrix[i][j].b-brilloInf);
+                dst_matrix[i][j].g = SAT(src_matrix[i][j].g-brilloInf);
+                dst_matrix[i][j].r = SAT(src_matrix[i][j].r-brilloInf);
+            } else {
+                dst_matrix[i][j].b = src_matrix[i][j].b;
+                dst_matrix[i][j].g = src_matrix[i][j].g;
+                dst_matrix[i][j].r = src_matrix[i][j].r;
+            }
+        }
+    }
+
+}
+
+/*
+void ReforzarBrillo_c(
+    uint8_t *src,
+    uint8_t *dst,
+    int width,
+    int height,
+    int src_row_size,
+    int dst_row_size,
+    int umbralSup,
+    int umbralInf,
+    int brilloSup,
+    int brilloInf)
+{
+    bgra_t (*src_matrix)[(src_row_size+3)/4] = (bgra_t (*)[(src_row_size+3)/4]) src;
+    bgra_t (*dst_matrix)[(dst_row_size+3)/4] = (bgra_t (*)[(dst_row_size+3)/4]) dst;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+
+            int b = (src_matrix[i][j].r + 2*src_matrix[i][j].g + src_matrix[i][j].b)/4;
 
             int brillo = 0;
 
@@ -40,3 +79,4 @@ void ReforzarBrillo_c(
     }
 
 }
+*/
